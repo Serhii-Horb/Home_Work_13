a285e3cc88cc5eadba0b81428ab59ecad1c2fddf
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-03-02 20:44:18 +0100

    Update Cat.java

--- src/main/java/org/example/Ex2/Cat.java
+++ src/main/java/org/example/Ex2/Cat.java
@@ -1,1 +1,57 @@
-package org.example.Ex2;public class Cat {    private String name;    private int age;    private double weight;    private String breed;    public Cat(String name, int age, double weight, String breed) {        this.name = name;        this.age = age;        this.weight = weight;        this.breed = breed;    }    public String getName() {        return name;    }    public void setName(String name) {        this.name = name;    }    public int getAge() {        return age;    }    public void setAge(int age) {        this.age = age;    }    public double getWeight() {        return weight;    }    public void setWeight(double weight) {        this.weight = weight;    }    public String getBreed() {        return breed;    }    public void setBreed(String breed) {        this.breed = breed;    }    @Override    public String toString() {        return "Cat {" +                "name = '" + name + '\'' +                ", age = " + age +                ", weight = " + weight +                ", breed = '" + breed + '\'' +                '}';    }}
+package org.example.Ex2;
+
+public class Cat {
+    private String name;
+    private int age;
+    private double weight;
+    private String breed;
+
+    public Cat(String name, int age, double weight, String breed) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat {" +
+                "name = '" + name + '\'' +
+                ", age = " + age +
+                ", weight = " + weight +
+                ", breed = '" + breed + '\'' +
+                '}';
+    }
+}
